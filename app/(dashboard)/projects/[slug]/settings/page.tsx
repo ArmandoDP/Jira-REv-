1d8fa05f2fc630900df9e5592b5cfdb39bf599fb
ArmandoDevelop <armandoshk1@gmail.com>
2026-06-10 16:51:08 -0600

    Add ProjectSettingsPage and NewProjectPage components for project management

---
+++ app/(dashboard)/projects/[slug]/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function ProjectSettingsPage() {
+  return (
+    <div>
+      <h1>Ajustes del proyecto</h1>
+    </div>
+  )
+}
---
+++ app/(dashboard)/projects/new/page.tsx
@@ -0,0 +1,7 @@
+export default function NewProjectPage() {
+  return (
+    <div>
+      <h1>Crear nuevo proyecto</h1>
+    </div>
+  )
+}
